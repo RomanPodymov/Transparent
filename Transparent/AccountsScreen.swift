f2a8c2dd6b1db0f2aa884ae2cb508ac2651f310e
Roman Podymov <podymfrombryansk@yandex.com>
2025-01-08 13:29:33 +0100

    Initial code

---
+++ Transparent/AccountsScreen.swift
@@ -0,0 +1,22 @@
+//
+//  AccountsScreen.swift
+//  Transparent
+//
+//  Created by Roman Podymov on 08/01/2025.
+//  Copyright © 2025 Transparent. All rights reserved.
+//
+
+import Kingfisher
+import SwiftUI
+import SwiftUI_CSS
+
+struct AccountsScreen: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                Text("Hello")
+            }
+            .padding(.vertical)
+        }
+    }
+}
---
+++ Transparent/TransparentApp.swift
@@ -0,0 +1,18 @@
+//
+//  TransparentApp.swift
+//  Transparent
+//
+//  Created by Roman Podymov on 08/01/2025.
+//  Copyright © 2025 Transparent. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct TransparentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AccountsScreen()
+        }
+    }
+}
